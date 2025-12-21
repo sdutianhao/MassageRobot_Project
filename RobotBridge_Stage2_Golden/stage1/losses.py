1b5445294ab0ca254cbe075a8570de234562f321
Zhang Tianhao <166203770+sdutianhao@users.noreply.github.com>
2025-12-21 08:38:23 +0800

    Delete RobotBridge_Stage2_Golden directory

--- RobotBridge_Stage2_Golden/stage1/losses.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-
-def depth_l1(pred, obs, mask):
-    return torch.mean(torch.abs(pred[mask] - obs[mask]))
-
-
-def depth_l2(pred, obs, mask):
-    return torch.mean((pred[mask] - obs[mask]) ** 2)
